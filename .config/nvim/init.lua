947113f566186f36eb7c0d904d7380a85f6d5e4a
MeteorSky <meteorskymail@gmail.com>
2025-06-26 09:36:43 +0800

    nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = { "utf-8", "gbk", "latin1" }
