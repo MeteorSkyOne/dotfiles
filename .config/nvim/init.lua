d654ab0b555e2d4a89f69818dee3d763d3256cd9
MeteorSky <meteorskymail@gmail.com>
2025-06-30 23:40:17 +0800

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,5 @@ require("config.lazy")
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
-vim.opt.fileencodings = { "utf-8", "gbk", "latin1" }
 vim.g.snacks_animate = false
+vim.opt.termguicolors = true
